ebd8f7f6034d24c04db5c0bc9d3871ec7a29972e
suka <khiemnguyen.hye@gmail.com>
2025-04-27 06:49:11 +0700

    Create create-user-authority-table.sql

---
+++ create-user-authority-table.sql
@@ -0,0 +1,51 @@
+USE `student_directory`;
+
+DROP TABLE IF EXISTS `authorities`;
+DROP TABLE IF EXISTS `users`;
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `enabled` tinyint NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `users`
+--
+
+INSERT INTO `users` 
+VALUES 
+('peppa','{noop}test123',1),
+('madam','{noop}test123',1),
+('aunty','{noop}test123',1);
+
+
+--
+-- Table structure for table `authorities`
+--
+
+CREATE TABLE `authorities` (
+  `username` varchar(50) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
+  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `authorities`
+--
+
+INSERT INTO `authorities` 
+VALUES 
+('peppa','ROLE_EMPLOYEE'),
+('madam','ROLE_EMPLOYEE'),
+('madam','ROLE_MANAGER'),
+('aunty','ROLE_EMPLOYEE'),
+('aunty','ROLE_MANAGER'),
+('aunty','ROLE_ADMIN');
+
